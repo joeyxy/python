4bc0dd3ca59b17e13e9aa20ca1bf7a9df61e75d0
joey <joeyxy83@gmail.com>
2013-12-23 22:31:47 +0800

    django test

---
+++ django/eshop/order/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+  
+  class Product(models.Model):  
+      name = models.CharField('product name', max_length=30)  
+          price = models.FloatField('price', default=10)  
+            
+                def __unicode__(self):  
+                        return "%s --> %f" %(self.name,self.price) 
+
+# Create your models here.
---
+++ django/nagiosapp/simplecmdb/simplecmdb/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('url(r'^api/gethost\.json$','hostinfo.views.gethosts')',
+    'url(r'^admin/',include(admin.site.urls))',
+    'url(r'^api/collect$','hostinfo.views.collect')'
+    # Examples:
+    # url(r'^$', 'simplecmdb.views.home', name='home'),
+    # url(r'^simplecmdb/', include('simplecmdb.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
