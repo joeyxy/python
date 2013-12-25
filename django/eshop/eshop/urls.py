7aa2e6a639bc52665e005f34b795ec7418414570
xiangyang <joeyxy83@gmail.com>
2013-12-25 01:17:44 -0500

    django test

--- django/eshop/eshop/urls.py
+++ django/eshop/eshop/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from order.views import hello,current_datetime,hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
+     (r'^hello/$',hello),
+     (r'^time/$',current_datetime),
+     (r'^time/plus/(\d{1,2})/$',hours_ahead),
 )
--- django/eshop/order/views.py
+++ django/eshop/order/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.http import Http404,HttpResponse
+import datetime
+from django.template.loader import get_template
+from django.template import Context
+
+def hello(request):
+	return HttpResponse("Hello world")
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	#html = "<html><body>It is now %s.</body></html>" % now
+	t = get_template('current_datetime.html')
+	html = t.render(Context({'current_date':now}))
+	return HttpResponse(html)
+
+def hours_ahead(request,offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now()+ datetime.timedelta(hours=offset)
+	html = "<html><body>In %s hour(s),it will be %s.</body></html>" % (offset,dt)
+	return HttpResponse(html)
