f74d5cabdb053ab69d8d64d68799d6c45582158a
root <root@slave.cdsztk.com>
2013-12-31 20:10:22 +0800

    add the init file

---
+++ py_init.sh
@@ -0,0 +1,32 @@
+[root@localhost python]# cat python_init.sh 
+#!/usr/bin/env bash
+#inital the python module install
+ 
+yum -y install gcc gcc-*
+sleep 1
+ 
+yum -y install python-devel
+sleep 1
+ 
+yum -y install sqlite-devel
+sleep1
+ 
+wget http://peak.telecommunity.com/dist/ez_setup.py 
+python ez_setup.py
+sleep 1
+easy_install pip
+sleep 1
+yum -y install python-devel
+sleep 1
+pip install termcolor
+sleep 2
+pip install processing
+sleep 1
+pip install pycrypto
+sleep 2
+pip install paramiko
+ 
+#install django
+pip install Django==1.5.1
+sleep 1
+pip install pysqlite
