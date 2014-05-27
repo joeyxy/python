622c12e2d193e1880e3cb92786eed03c91021890
joey <joeyxy83@gmail.com>
2014-05-27 16:51:08 +0800

    add warrantycheck.py scripts

--- crawler/precheck.sh
+++ crawler/precheck.sh
@@ -4,4 +4,5 @@ yum -y install python-devel python-setuptools
 sleep 1;
 easy_install BeautifulSoup
 sleep 1;
-
+wget https://github.com/joeyxy/python/blob/master/crawler/warrantycheck.py
+chmod +x warrantycheck.py
