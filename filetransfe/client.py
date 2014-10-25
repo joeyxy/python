d803d97f8f746a293998bd4123030ed8782eaadf
joey <joeyxy83@gmail.com>
2014-10-25 11:48:36 +0000

    file transfer

---
+++ filetransfe/client.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import socket, time  
+class MyClient:     
+    def __init__(self):  
+        print 'Prepare for connecting...'     
+    def connect(self):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        sock.connect(('54.169.1.224', 8890))  
+        #connect to the server
+        sock.sendall('Hi, server')  
+        self.response = sock.recv(8192)  
+        print 'Server:', self.response     
+        #instance's attribute
+        self.s = raw_input("Connect to the server?(y/n):")  
+        if self.s == 'y':  
+            while True:  
+                self.name = raw_input('Server: input our name:')  
+                sock.sendall('name:' + self.name.strip())  
+                self.response = sock.recv(8192)  
+                if self.response == 'valid':  
+                    break 
+                else:  
+                    print 'Server: Invalid username'    
+            while True:  
+                self.pwd = raw_input('Server: input our password:')  
+                sock.sendall('pwd:' + self.pwd.strip())  
+                self.response = sock.recv(8192)                 
+                if self.response == 'valid':  
+                    print 'Welcome'+self.name+'\n'+'please wait...'
+                    #******************************
+                    #we need to change and get the list_dir which is contained by
+                    #a list sent by the server.
+                    #get the "self.response" and print the content.
+                    #input the file wanted and open it below.
+                    #*****************************************
+                    self.response = sock.recv(8192)
+                    print "**FILE LIST**"
+                    file_string = self.response
+                    file_list=file_string[1:-1]
+                    self.count=1
+                    for item in file_string[1:-1].split(','):
+                        print "[%d]:" %self.count,
+                        print item.strip()[1:-1]
+                        self.count+=1
+                   
+                    print "please choose a file to download..."                   
+                    while True:
+                        self.file_to_down = raw_input('you choose:')
+                        sock.sendall(self.file_to_down.strip())
+                        break                     
+                    f = open(self.file_to_down, 'wb')  
+                    while True:  
+                        data = sock.recv(1024)  
+                        if data == 'EOF':  
+                            break 
+                        f.write(data)                              
+                    f.flush()  
+                    f.close()     
+                    print 'download finished'  
+                    break 
+                else:  
+                    print 'Server: Invalid password'  
+                  
+ 
+        sock.sendall('bye')  
+        sock.close()  
+        print 'Disconnected'  
+ 
+if __name__ == '__main__':  
+    client = MyClient()  
+    client.connect() 
---
+++ filetransfe/server.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import SocketServer, time 
+import os
+import yaml
+class MyServer(SocketServer.BaseRequestHandler):  
+   ##what the reason that the func of init did not work
+    #def __init__(self):
+        #try:
+            #config = yaml.load(file('config.yaml',"r"))
+        #except yaml.YAMLError:
+            #print "error in configuration file"
+        #self.user = config['userinfo']['username']
+        #self.pwd = config['userinfo']['password']
+        #self.root_path = config['root']   
+        #print ">>>**the process of reading config has done**"
+        #print self.user
+        #print self.pwd
+        #print self.root_path
+       
+    def init_config(self):
+        try:
+            config = yaml.load(file('config.yaml',"r"))
+        except yaml.YAMLError:
+            print "error in configuration file"
+        self.user = config['userinfo']['username']
+        self.pwd = config['userinfo']['password']
+        self.root_path = config['root']   
+        print ">>>**the process of reading config has done**"
+        print self.user
+        print self.pwd
+        print self.root_path
+       
+    def handle(self):  
+        print '>>>Connected from', self.client_address           
+        self.init_config()        
+        while True:  
+            receivedData = self.request.recv(8192)  
+            if not receivedData:  
+                continue 
+            #build a connection
+            elif receivedData == 'Hi, server':  
+                self.request.sendall('hi, client')                      
+            elif receivedData.startswith('name'):  
+                #the last element of list is the name
+                self.clientName = receivedData.split(':')[-1]   
+                if self.user==self.clientName:  
+                    self.request.sendall('valid')  
+                else:  
+                    self.request.sendall('invalid')                          
+            elif receivedData.startswith('pwd'):  
+                self.clientPwd = receivedData.split(':')[-1]
+                if self.pwd==self.clientPwd:
+                    self.request.sendall('valid')  
+                    time.sleep(5)  
+                    #***************************************
+                    #when the authentication has done,then send the dir
+                    #the client choose one and the server reply it
+                    #D:\act\python_project\act_a_d\server
+                    dirfiles=os.listdir(self.root_path)
+                    filedata=""
+                    print ">>>**the dir has following files**"
+                    for item in dirfiles:
+                        print item
+                        filedata+=item
+                    self.request.sendall(str(dirfiles))                  
+                    filename = self.request.recv(8192) 
+                    print "receive the user's choose:"+filename
+                    sfile = open(filename,'rb')
+                    #***************************************                        
+                    while True:  
+                        data = sfile.read(1024)  
+                        if not data:  
+                            break 
+                        #divide the file into segments and send respectively.
+                        while len(data) > 0:  
+                            intSent = self.request.send(data)  
+                            data = data[intSent:]     
+                    time.sleep(3)  
+                    #destroy the connect
+                    self.request.sendall('EOF')  
+                else:  
+                    self.request.sendall('invalid')                          
+            elif receivedData == 'bye':  
+                break    
+        self.request.close()              
+        print 'Disconnected from', self.client_address  
+        print 
+ 
+if __name__ == '__main__':  
+    print 'Server is started\nwaiting for connection...\n'  
+    srv = SocketServer.ThreadingTCPServer(('192.168.10.13', 50000), MyServer)  
+    srv.serve_forever()  
